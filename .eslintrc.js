96b5052128f4154befcb54fe4f7d0e2d1654fa35
xdoo <claus.straube@gmail.com>
2022-02-15 21:21:02 +0100

    c&p from linter docu is not good enough for the linter ;)

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,8 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    "vue/multi-word-component-names": ["error", {
-      "ignores": ['index', 'default']
+    'vue/multi-word-component-names': ['error', {
+      'ignores': ['index', 'default']
     }]
   }
 }
