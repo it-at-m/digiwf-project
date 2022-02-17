656f3bfd937b9c788594a2d0c4e6b6cb28e7e97c
xdoo <xdoo@users.noreply.github.com>
2022-02-17 07:31:16 +0000

    deploy: 60a2e488a3ae4ab89a1048f600df62334edbb102

--- _nuxt/static/1644996641/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdigiwf-project\u002F_nuxt\u002Fstatic\u002F1644996641",layout:"landing-layout",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdigiwf-project\u002F",assetsPath:"\u002Fdigiwf-project\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5806c7b3"}}}}(null));
