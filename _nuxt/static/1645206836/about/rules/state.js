780afdd41c931f295490530b97fb344f620be56d
dominikhorn93 <dominikhorn93@users.noreply.github.com>
2022-02-18 18:11:43 +0000

    deploy: 355dd13bec0c9dd1e40544f1ddcde0963e8428a6

--- _nuxt/static/1645206836/about/rules/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645206836",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout\u002Frules",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c4153344"}}}}(null));
--- _nuxt/static/1645206836/resources/documentation/architecture/handlingfiles/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645206836",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresources\u002Fdocumentation\u002Farchitecture\u002Fhandlingfiles",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c4153344"}}}}(null));
