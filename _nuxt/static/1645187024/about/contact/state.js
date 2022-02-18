cd86d3cabb96c11ba69931bdbdcea415c74badbd
xdoo <xdoo@users.noreply.github.com>
2022-02-18 12:26:07 +0000

    deploy: b3dd964c7ccd471f8129186b2a425a31cbc493ad

--- _nuxt/static/1645187024/about/contact/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdigiwf-project\u002F_nuxt\u002Fstatic\u002F1645187024",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout\u002Fcontact",config:{_app:{basePath:"\u002Fdigiwf-project\u002F",assetsPath:"\u002Fdigiwf-project\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6a75853d"}}}}(null));
