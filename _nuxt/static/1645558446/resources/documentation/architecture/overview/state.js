0eb97cbcdac9e3277b173214f352aa75cbced4ef
xdoo <xdoo@users.noreply.github.com>
2022-02-22 19:34:10 +0000

    deploy: 74f551e46f489e8426739dd091694e95f4b54a38

---
+++ _nuxt/static/1645558446/resources/documentation/architecture/overview/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645558446",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresources\u002Fdocumentation\u002Farchitecture\u002Foverview",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1d7690a0"}}}}(null));
