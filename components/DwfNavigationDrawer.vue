3e08383277d8b19e3fdcf1e2bb8e981326ce30fb
xdoo <claus.straube@gmail.com>
2022-03-11 10:51:31 +0100

    Making linter happy 4. see #51

--- components/DwfNavigationDrawer.vue
+++ components/DwfNavigationDrawer.vue
@@ -2,15 +2,21 @@
   <v-navigation-drawer v-model="drawer" v-bind="$attrs">
     <v-list-item class="px-2">
       <v-list-item-icon>
-        <v-icon>{{ navigation[0].categoryIcon }}</v-icon>
+        <v-icon>
+          {{ navigation[0].categoryIcon }}
+        </v-icon>
       </v-list-item-icon>
-      <v-list-item-title>{{ navigation[0].category }}</v-list-item-title>
+      <v-list-item-title>
+        {{ navigation[0].category }}
+      </v-list-item-title>
     </v-list-item>
-    <v-divider></v-divider>
+    <v-divider />
     <v-list nav dense>
       <v-list-item v-for="nav in navigation" :key="nav.path" nuxt :to="nav.path">
         <v-list-item-content>
-          <v-list-item-title>{{ nav.title }}</v-list-item-title>
+          <v-list-item-title>
+            {{ nav.title }}
+          </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
     </v-list>
