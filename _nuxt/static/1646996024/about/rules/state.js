af616c070bf0afb1c155083761d5f019b5b42b8a
xdoo <xdoo@users.noreply.github.com>
2022-03-15 09:34:42 +0000

    deploy: 704775250cb7b79df97a338026fec75b5e49c058

--- _nuxt/static/1646996024/about/rules/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646996024",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout\u002Frules",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8a325e9d"}}}}(null));
