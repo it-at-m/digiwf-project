1a37b1fd9273877028dcd031a33d7fb75865784c
xdoo <xdoo@users.noreply.github.com>
2022-03-17 07:24:50 +0000

    deploy: c039bd665e6576acae4ac10e80a81d94a1bf0d03

--- _nuxt/static/1647336879/resources/documentation/architecture/handlingfiles/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647336879",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresources\u002Fdocumentation\u002Farchitecture\u002Fhandlingfiles",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6e6619d8"}}}}(null));
--- _nuxt/static/1647336879/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647336879",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6e6619d8"}}}}(null,"\u002F"));
