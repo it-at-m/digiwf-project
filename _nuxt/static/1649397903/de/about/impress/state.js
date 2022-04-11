4ad9e26adc725884840639cbe948c6c94732d28a
xdoo <xdoo@users.noreply.github.com>
2022-04-11 09:11:07 +0000

    deploy: 746f6916829e480355bdc755d92794d714f29c89

--- _nuxt/static/1649397903/de/about/impress/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649397903",layout:"default",error:a,serverRendered:true,routePath:"\u002Fde\u002Fabout\u002Fimpress",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"81b6409e"}}}}(null));
