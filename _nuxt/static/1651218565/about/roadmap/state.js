fde42118da3c1cd223a2408cc59edfff9ed8a077
xdoo <xdoo@users.noreply.github.com>
2022-05-02 05:47:19 +0000

    deploy: 88e301a3cf70dfc8c34d8477d79764a68cd03f7f

--- _nuxt/static/1651218565/about/roadmap/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651218565",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout\u002Froadmap",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bedb185e"}}}}(null));
---
+++ _nuxt/static/1651470431/de/resources/documentation/concept/selfservice/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651470431",layout:"default",error:a,serverRendered:true,routePath:"\u002Fde\u002Fresources\u002Fdocumentation\u002Fconcept\u002Fselfservice",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2edbcbee"}}}}(null));
