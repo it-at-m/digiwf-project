e15aa217d1aaec5adc452e59c05b062e8aede6c0
xdoo <xdoo@users.noreply.github.com>
2022-05-03 06:09:53 +0000

    deploy: 433fba160b87a4c0aee39469f31451e849c4baa3

--- _nuxt/static/1651557888/about/vision/vision/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651557888",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout\u002Fvision\u002Fvision",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"996859f9"}}}}(null));
