5460285d7a452d910e4626f5e496570b4648e1ff
xdoo <xdoo@users.noreply.github.com>
2022-05-19 13:54:42 +0000

    deploy: f2ccd34530b847200e3dbfc14e88be3f295bcc8b

---
+++ _nuxt/static/1652968471/about/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652968471",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout\u002Fcontact",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c0eae652"},http:{browserBaseURL:b}}}}(null,"\u002F"));
