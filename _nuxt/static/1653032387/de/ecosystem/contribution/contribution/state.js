ed98bdde867909ab0d021f16a9eccea7c5ba6e83
xdoo <xdoo@users.noreply.github.com>
2022-05-20 07:42:31 +0000

    deploy: 2585cce593a7a30234365fd51fba78823ca7df79

--- _nuxt/static/1653032387/de/ecosystem/contribution/contribution/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653032387",layout:"default",error:a,serverRendered:true,routePath:"\u002Fde\u002Fecosystem\u002Fcontribution\u002Fcontribution",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c07adf89"},http:{browserBaseURL:b}}}}(null,"\u002F"));
---
+++ _nuxt/static/1653032542/de/about/rules/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653032542",layout:"default",error:a,serverRendered:true,routePath:"\u002Fde\u002Fabout\u002Frules",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a9713535"},http:{browserBaseURL:b}}}}(null,"\u002F"));
