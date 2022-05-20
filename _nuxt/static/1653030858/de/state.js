2592e19d67eea5b33fb9191160548e3bbcd0e5a4
xdoo <xdoo@users.noreply.github.com>
2022-05-20 07:14:29 +0000

    deploy: 2b286df2908c38e265e8dd6525d54be18c16ea07

---
+++ _nuxt/static/1653030858/de/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653030858",layout:"default",error:a,serverRendered:true,routePath:"\u002Fde",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5ebedae1"},http:{browserBaseURL:b}}}}(null,"\u002F"));
