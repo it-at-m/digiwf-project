5820c6fe751b540cd800f217cd2e25468217c83f
lmoesle <lmoesle@users.noreply.github.com>
2022-07-22 07:55:35 +0000

    deploy: 2d5a57f2a19fd5e4417f4abf9bbbf6046efa1693

--- _nuxt/static/1655965304/about/contact/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655965304",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout\u002Fcontact",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1eb04b30"},http:{browserBaseURL:b}}}}(null,"\u002F"));
