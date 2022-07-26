be8948411016948e5bbf5dba7d6efc8d58502cac
klml <klml@users.noreply.github.com>
2022-07-26 07:07:17 +0000

    deploy: 376f0c8afd95df06cc42c6a58b9c77a131e37e21

---
+++ _nuxt/static/1658819222/about/vision/vision/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658819222",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout\u002Fvision\u002Fvision",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f17b78a5"},http:{browserBaseURL:b}}}}(null,"\u002F"));
