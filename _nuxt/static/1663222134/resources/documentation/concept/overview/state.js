fcd7a9b465aeed71e7035f9252bb965ac0ce7b8d
xdoo <xdoo@users.noreply.github.com>
2022-09-15 06:18:21 +0000

    deploy: 1630982636e8a55fc56a221dae95e88a318eac0c

--- _nuxt/static/1663222134/resources/documentation/concept/overview/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663222134",layout:"default",error:a,serverRendered:true,routePath:"\u002Fresources\u002Fdocumentation\u002Fconcept\u002Foverview",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4839790d"},http:{browserBaseURL:b}}}}(null,"\u002F"));
---
+++ _nuxt/static/1663222690/about/rules/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663222690",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout\u002Frules",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"963af693"},http:{browserBaseURL:b}}}}(null,"\u002F"));
