1c9d976a8f9abbd1ce23c657797c0e1f4646b427
darenegade <darenegade@users.noreply.github.com>
2022-11-23 09:25:50 +0000

    deploy: cea75a08b4bf0594261bd86fa491211ccc5770ff

---
+++ _nuxt/static/1669195536/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1669195536",layout:"default",error:b,serverRendered:true,routePath:a,config:{_app:{basePath:a,assetsPath:"\u002F_nuxt\u002F",cdnURL:b},content:{dbHash:"58d51e04"},http:{browserBaseURL:a}}}}("\u002F",null));
